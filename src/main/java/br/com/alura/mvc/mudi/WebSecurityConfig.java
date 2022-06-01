a7aa2c69dfbe1b803f4344ccc081d89c35638e1d
Henrique Naimaier <coubron@yahoo.com.br>
2022-06-01 12:07:32 -0300

    Added basic security configuration and one in memory user

---
+++ src/main/java/br/com/alura/mvc/mudi/WebSecurityConfig.java
@@ -0,0 +1,40 @@
+package br.com.alura.mvc.mudi;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter{
+	
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		
+		http
+		.authorizeRequests()
+			.anyRequest().authenticated()
+		.and()
+			.httpBasic(); //autenticacao usando popup do navegador
+	}
+	
+	@Bean
+	@Override
+	protected UserDetailsService userDetailsService() {
+		
+		//esse método está deprecated porque não é seguro usá-lo em produção.
+		UserDetails user = User.withDefaultPasswordEncoder()
+			.username("user")
+			.password("user")
+			.roles("ADM")
+			.build();
+		
+		return new InMemoryUserDetailsManager(user);
+	}
+}
