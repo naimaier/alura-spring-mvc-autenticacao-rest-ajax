39e7fb2791f195422731332be6f6079557cbaf94
Henrique Naimaier <coubron@yahoo.com.br>
2022-06-06 13:28:21 -0300

    FIXED cache error by adding name to cache

--- src/main/java/br/com/alura/mvc/mudi/repository/PedidoRepository.java
+++ src/main/java/br/com/alura/mvc/mudi/repository/PedidoRepository.java
@@ -15,7 +15,7 @@ import br.com.alura.mvc.mudi.model.StatusPedido;
 @Repository
 public interface PedidoRepository extends JpaRepository<Pedido, Long>{
 
-	@Cacheable
+	@Cacheable("pedidos")
 	List<Pedido> findByStatus(StatusPedido status, Pageable paginacao);
 
 	@Query("SELECT p FROM Pedido p JOIN p.user u WHERE u.username = :username")
